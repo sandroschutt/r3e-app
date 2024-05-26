eff603a983d1197737e37bad7b744b8cc06639e0
Gabriel Schütt de Almeida <gabrielschuttdealmeida@gmail.com>
2024-05-26 17:36:22 -0300

    feat: part2

--- src/components/UserHeader/index.js
+++ src/components/UserHeader/index.js
@@ -3,8 +3,24 @@ import { Col, Row } from "react-bootstrap";
 import SearchForm from "../forms/SearchForm";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { NotificationsModal } from "../Modals";
+import { useState } from "react";
 
 export default function UserHeader(props) {
+
+    const [showNotifications, setShowNotifications] = useState(false);
+    const handleNotification = () => {
+        setShowNotifications(!showNotifications);
+    }
+
+    const NotificationModal = () => {
+        return (
+            <>
+                <NotificationsModal/>
+            </>
+        );
+    }
+
     return (
         <header>
             <Row className="p-0 justify-content-between align-items-center row-gap-1 mb-lg-5">
@@ -15,8 +31,7 @@ export default function UserHeader(props) {
                     <SearchForm />
                 </Col>
                 <Col className="col-1 text-end pe-0">
-                    <FontAwesomeIcon className="notifications" icon={faBell} onClick={() =>
-                     NotificationsModal() />
+                    <FontAwesomeIcon className="notifications" icon={faBell} onClick={() => {handleNotification()}} />
                 </Col>
             </Row>
         </header>
