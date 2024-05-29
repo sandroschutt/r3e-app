91851a66fec39031901a7b449d0adec2eb3e67af
sandro schutt <schuttsandro@outlook.com>
2024-05-29 13:08:56 -0300

    Fix dashboard roles

--- src/pages/User/Dashboard/index.js
+++ src/pages/User/Dashboard/index.js
@@ -3,12 +3,22 @@ import { useUserDataContext } from "../../../context/UserDataContext";
 import { Col } from "react-bootstrap";
 import UserDashboard from "../../../components/Dashboards/User";
 import AdminDashboard from "../../../components/Dashboards/Admin";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
   const { userData, updateUserData } = useUserDataContext();
+  const [role, setRole] = useState("");
 
+  useEffect(() => {
+    if(userData.user !== undefined && role === "") {
+      setRole(userData.user.role);
+    }
+  }, [userData, role])
+  
   function renderDashboard(role) {
     switch(role) {
+      default:
+        return <UserDashboard />
       case "Admin":
         return <AdminDashboard />
       case "Cliente":
@@ -18,11 +28,11 @@ export default function Dashboard() {
 
   return (
     <div className="profile-view">
-      <Col className="dashboard-header">
+      <Col className="dashboard-header mb-5">
         <h1 className="w-100">Painel de controle</h1>
       </Col>
       {
-        renderDashboard("Admin")
+        renderDashboard(role)
       }
     </div>
   );
