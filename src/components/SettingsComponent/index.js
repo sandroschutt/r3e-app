50dff3142826a3d1b275d8c8af8efc965f05be4f
sandroschutt <dev.sandroschutt@gmail.com>
2024-09-27 12:17:10 -0300

    Prepare UserSettings view for sending data to API

--- src/components/SettingsComponent/index.js
+++ src/components/SettingsComponent/index.js
@@ -1,171 +1,219 @@
-import { Col, Row } from 'react-bootstrap'
-import './style.scss'
-import Form from 'react-bootstrap/Form'
-import { useState } from 'react'
+import { Button, Col, Row } from "react-bootstrap";
+import "./style.scss";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
-export function SettingsComponent () {
-  const [isVisible, setIsVisible] = useState(false)
-  const [showLocalization, setShowLocalization] = useState(false)
-  const [showEmail, setShowEmail] = useState(false)
-  const [anyoneCanContact, setAnyoneCanContact] = useState(false)
+export function SettingsComponent() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [showLocalization, setShowLocalization] = useState(true);
+  const [showContactInfo, setShowContactInfo] = useState(true);
+  const [showNotifications, setShowNotifications] = useState(true);
+  const [anyoneCanContact, setAnyoneCanContact] = useState(true);
 
-  function handleVisibility () {
-    setIsVisible(!isVisible)
+  const settings = {
+    visibleLocation: "",
+    visiblePhone: "",
+    visibleMail: "",
+    notifications: "",
+    recieveMessages: "",
+  };
+
+  function handleVisibility() {
+    setIsVisible(!isVisible);
+
+    console.log(isVisible);
   }
 
-  function handleShowLocalization () {
-    setShowLocalization(!showLocalization)
+  function handleShowLocalization() {
+    setShowLocalization(!showLocalization);
   }
-  function handleShowEmail () {
-    setShowEmail(!showEmail)
+
+  function handleShowContactInfo() {
+    setShowContactInfo(!showContactInfo);
   }
-  function handleAnyoneCanContact () {
-    setAnyoneCanContact(!anyoneCanContact)
+
+  function handleShowNotifications() {
+    setShowNotifications(!showNotifications);
+  }
+
+  function handleAnyoneCanContact() {
+    setAnyoneCanContact(!anyoneCanContact);
+  }
+
+  function handleUpdateSettings() {
+    if (isVisible) {
+      settings.visibleLocation = showLocalization;
+      settings.visiblePhone = showContactInfo;
+      settings.visibleMail = showContactInfo;
+      settings.notifications = showNotifications;
+      settings.recieveMessages = anyoneCanContact;
+    } else {
+      settings.visibleLocation = false;
+      settings.visiblePhone = false;
+      settings.visibleMail = false;
+      settings.notifications = false;
+      settings.recieveMessages = false;
+    }
+
+    console.log(settings);
   }
 
   return (
     <div>
-      <Form>
-        <h5>Privacidade</h5>
-        <Col className='col-6'>
-          <Row className='flex-row justify-content mb-0'>
-            <Col className='col-6 text-start mr-0 my-0'>
-              <p> Visivilidade</p>
-            </Col>
-            <Col className='col-4 text-end mx-0'>
-              <p className='mb-0 mr-2'>
-                {isVisible ? 'Visível' : 'Invisível '}
-              </p>
-            </Col>
-            <Col className='col-1 text-end mx-0'>
-              <Form.Check
-                type='switch'
-                id='visibility-swith'
-                onClick={() => {
-                  handleVisibility()
-                }}
-              />
-            </Col>
-          </Row>
-          <hr className='mt-0' />
-        </Col>
+      <Form
+        className="mb-5"
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleUpdateSettings();
+        }}
+      >
+        <h5 className="mb-4">Privacidade</h5>
+        <Row className="flex-row justify-content w-50 w-sm-100 w-xs-50 mb-0">
+          <Col className="col-6 text-start mr-0 my-0">
+            <p> Visivilidade</p>
+          </Col>
+          <Col className="col-4 text-end mx-0">
+            <p className="mb-0 mr-2">{isVisible ? "Visível" : "Invisível "}</p>
+          </Col>
+          <Col className="col-1 text-end mx-0">
+            <Form.Check
+              type="switch"
+              id="visibility-swith"
+              checked={isVisible}
+              isValid
+              onChange={() => {
+                handleVisibility();
+              }}
+            />
+          </Col>
+        </Row>
+        <hr className="mt-0 w-50 w-sm-100 w-xs-100" />
 
-        <Col className='col-6'>
-          <Row className='flex-row justify-content mb-0'>
-            <Col className='col-6 text-start  mr-5 my-0'>
-              <p>Outros usuários podem ver minha localização ?</p>
-            </Col>
-            <Col className='col-4 text-end mx-0'>
-              <p className='mb-0 mr-2'>{showLocalization ? 'Sim' : 'Não'}</p>
-            </Col>
-            <Col className='col-1 text-end mx-0'>
-              <Form.Check // prettier-ignore
-                type='switch'
-                id='show-localization-switch'
-                onClick={() => {
-                  handleShowLocalization()
-                }}
-              />
-            </Col>
-          </Row>
-          <hr className='mt-0' />
-        </Col>
+        <Row className="flex-row justify-content w-50 w-sm-100 w-xs-50 mb-0">
+          <Col className="col-6 text-start  mr-5 my-0">
+            <p>Outros usuários podem ver minha localização ?</p>
+          </Col>
+          <Col className="col-4 text-end mx-0">
+            <p className="mb-0 mr-2">{showLocalization ? "Sim" : "Não"}</p>
+          </Col>
+          <Col className="col-1 text-end mx-0">
+            <Form.Check // prettier-ignore
+              type="switch"
+              id="show-localization-switch"
+              disabled={isVisible === false ? true : false}
+              checked={showLocalization}
+              onChange={() => {
+                handleShowLocalization();
+              }}
+            />
+          </Col>
+        </Row>
+        <hr className="mt-0 w-50 w-sm-100 w-xs-100" />
 
-        <Col className='col-6'>
-          <Row className='flex-row justify-content mb-0'>
-            <Col className='col-6 text-start mr-5 my-0'>
-              <p className='fs-12'>
-                Outros usuários podem ver meu telefone e email ?{' '}
-              </p>
-            </Col>
-            <Col className='col-4 text-end text-end mx-0'>
-              <p className='mb-0 mr-2'>{showEmail ? 'Sim' : 'Não'}</p>
-            </Col>
-            <Col className='col-1 text-end mx-0'>
-              <Form.Check // prettier-ignore
-                type='switch'
-                id='show-email-switch'
-                onClick={() => {
-                  handleShowEmail()
-                }}
-              />
-            </Col>
-          </Row>
-          <hr className='mt-0' />
-        </Col>
+        <Row className="flex-row justify-content w-50 w-sm-100 w-xs-50 mb-0">
+          <Col className="col-6 text-start mr-5 my-0">
+            <p className="fs-12">Outros usuários podem ver meu email? </p>
+          </Col>
+          <Col className="col-4 text-end text-end mx-0">
+            <p className="mb-0 mr-2">{showContactInfo ? "Sim" : "Não"}</p>
+          </Col>
+          <Col className="col-1 text-end mx-0">
+            <Form.Check // prettier-ignore
+              type="switch"
+              id="show-email-switch"
+              disabled={isVisible === false ? true : false}
+              checked={showContactInfo}
+              onChange={() => {
+                handleShowContactInfo();
+              }}
+            />
+          </Col>
+        </Row>
+        <hr className="mt-0 w-50 w-sm-100 w-xs-100" />
 
-        <Col className='col-6'>
-          <Row className='flex-row justify-content mb-0'>
-            <Col className='col-6 text-start mr-5 my-0'>
-              <p> Qualquer usuário pode entrar em contato comigo ?</p>
-            </Col>
-            <Col className='col-4 text-end mx-0'>
-              <p className='mb-0 mr-2'>{anyoneCanContact ? 'Sim' : 'Não'}</p>
-            </Col>
-            <Col className='col-1 text-end mx-0'>
-              <Form.Check // prettier-ignore
-                type='switch'
-                id='anyone-can-contact-switch'
-                onClick={() => {
-                  handleAnyoneCanContact()
-                }}
-              />
-            </Col>
-          </Row>
-          <hr className='my-0'/>
-        </Col>
+        <Row className="flex-row justify-content w-50 w-sm-100 w-xs-50 mb-0">
+          <Col className="col-6 text-start mr-5 my-0">
+            <p>Receber notificações?</p>
+          </Col>
+          <Col className="col-4 text-end mx-0">
+            <p className="mb-0 mr-2">{showNotifications ? "Sim" : "Não"}</p>
+          </Col>
+          <Col className="col-1 text-end mx-0">
+            <Form.Check // prettier-ignore
+              type="switch"
+              id="anyone-can-contact-switch"
+              disabled={isVisible === false ? true : false}
+              checked={showNotifications}
+              onChange={() => {
+                handleShowNotifications();
+              }}
+            />
+          </Col>
+        </Row>
+        <hr className="mt-0 w-50 w-sm-100 w-xs-100" />
+
+        <Row className="flex-row justify-content w-50 w-sm-100 w-xs-50 mb-0">
+          <Col className="col-6 text-start mr-5 my-0">
+            <p> Qualquer usuário pode entrar em contato comigo ?</p>
+          </Col>
+          <Col className="col-4 text-end mx-0">
+            <p className="mb-0 mr-2">{anyoneCanContact ? "Sim" : "Não"}</p>
+          </Col>
+          <Col className="col-1 text-end mx-0">
+            <Form.Check // prettier-ignore
+              type="switch"
+              id="anyone-can-contact-switch"
+              disabled={isVisible === false ? true : false}
+              checked={anyoneCanContact}
+              onChange={() => {
+                handleAnyoneCanContact();
+              }}
+            />
+          </Col>
+        </Row>
+        <hr className="mt-0 w-50 w-sm-100 w-xs-100" />
+        <Button variant="outline-success" type="submit">
+          Salvar
+        </Button>
       </Form>
-      <h5 className='my-4'>Conta</h5>
-      <Col className='col-8'>
-        <Row>
-          <Col className='col-9 text-start p-0'>
-            <button type='button' class='btn btn-link' style={{color: '#000000', textDecoration: 'none' }}>
-              Redefinir Senha
-            </button>
-            <hr className='my-2' />
-          </Col>
-        </Row>
-      </Col>
-      <Col className='col-8'>
-        <Row>
-          <Col className='col-9 text-start p-0'>
-            <button type='button' class='btn btn-link' style={{color: '#000000', textDecoration: 'none' }}>
-              Mudar e-mail principal
-            </button>
-            <hr className='my-2' />
-          </Col>
-        </Row>
-      </Col>
-      <Col className='col-8'>
-        <Row>
-          <Col className='col-9 text-start p-0'>
-            <button type='button' class='btn btn-link' style={{color: '#000000', textDecoration: 'none' }}>
-              Mudar e-mail de recuperação de senha
-            </button>
-            <hr className='my-2' />
-          </Col>
-        </Row>
-      </Col>
-      <Col className='col-8'>
-        <Row>
-          <Col className='col-9 text-start p-0'>
-            <button type='button' class='btn btn-link' style={{color: '#000000', textDecoration: 'none' }}>
-              Solicitar cópia dos meus dados
-            </button>
-            <hr className='my-2' />
-          </Col>
-        </Row>
-      </Col>
-      <Col className='col-8'>
-        <Row>
-          <Col className='col-9 text-start p-0'>
-            <button type='button' class='btn btn-link' style={{color: '#ce0707', textDecoration: 'none' }}>
-              Excluir conta do R3E
-            </button>
-            <hr className='my-2' />
-          </Col>
-        </Row>
-      </Col>
+
+      <h5 className="my-4">Conta</h5>
+      <Row className="flex-row justify-content w-50 w-sm-100 w-xs-50 mb-0">
+        <Col>
+          <button
+            type="button"
+            className="btn btn-link"
+            style={{ color: "#000000", textDecoration: "none" }}
+          >
+            Redefinir Senha
+          </button>
+          <hr className="my-2" />
+        </Col>
+      </Row>
+      <Row className="flex-row justify-content w-50 w-sm-100 w-xs-50 mb-0">
+        <Col>
+          <button
+            type="button"
+            className="btn btn-link"
+            style={{ color: "#000000", textDecoration: "none" }}
+          >
+            Solicitar cópia dos meus dados
+          </button>
+          <hr className="my-2" />
+        </Col>
+      </Row>
+      <Row className="flex-row justify-content w-50 w-sm-100 w-xs-50 mb-0">
+        <Col>
+          <button
+            type="button"
+            className="btn btn-link"
+            style={{ color: "#ce0707", textDecoration: "none" }}
+          >
+            Excluir conta do R3E
+          </button>
+          <hr className="my-2" />
+        </Col>
+      </Row>
     </div>
-  )
+  );
 }
