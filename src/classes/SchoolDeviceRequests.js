b0c325d7a004f19f678237fccefead90cbb74c40
sandroschutt <dev.sandroschutt@gmail.com>
2024-10-11 14:03:49 -0300

    Finishes table and table actions

--- src/classes/SchoolDeviceRequests.js
+++ src/classes/SchoolDeviceRequests.js
@@ -22,21 +22,100 @@ export default class SchoolDeviceRequets {
 
   static async update() {}
 
-  static async delete() {}
+  /**
+   * Deletes all School's DeviceRequests
+   *
+   * @param {String|Number} id The SchoolDeviceRequest's id
+   * */
+  static async delete(id) {
+    axios
+      .post(
+        Api.endpoint(`school-device-requests/${id}/delete`),
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        if (response.status !== 200) throw new Error("Erro na requisição");
+        alert(`Pedido de doação #${id} excluído com sucesso!`);
+      })
+      .catch((error) => {
+        alert(error.message);
+        console.error(error);
+      });
+  }
 
-/**
- * Retrieves all School's DeviceRequests
- * 
- * @param {String|Number} id The School's id
- * @param {CallableFunction} setSchoolDeviceRequets A callback function for setting the SchoolDeviceRequests' state
- * */   
+  /**
+   * Retrieves all School's DeviceRequests
+   *
+   * @param {String|Number} id The School's id
+   * @param {CallableFunction} setSchoolDeviceRequets A callback function for setting the SchoolDeviceRequests' state
+   * */
   static async getSchoolRequests(id, setSchoolDeviceRequets) {
     axios
       .get(Api.endpoint(`school-device-requests/school/${id}`))
       .then((response) => {
         if (response.status !== 200) throw new Error("Erro na requisição");
         setSchoolDeviceRequets(response.data);
-        console.log(response.data);
+      })
+      .catch((error) => {
+        alert(error.message);
+        console.error(error);
+      });
+  }
+
+  /**
+   * Approves a single SchoolDeviceRequest
+   *
+   * @param {String|Number} id The SchoolDeviceRequest's id
+   * */
+  static async approve(id) {
+    axios
+      .post(
+        Api.endpoint(`school-device-requests/${id}/approve`),
+        { approve: 1 },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        if (response.status !== 200)
+          throw new Error(`Falha ao aprovar o pedido de doação.`);
+        alert(`Pedido de doação ${id} aprovado com sucesso!`);
+        window.location.reload();
+      })
+      .catch((error) => {
+        alert(error.message);
+        console.error(error);
+      });
+  }
+
+  /**
+   * Reproves a single SchoolDeviceRequest
+   *
+   * @param {String|Number} id The SchoolDeviceRequest's id
+   * */
+  static async reprove(id) {
+    axios
+      .post(
+        Api.endpoint(`school-device-requests/${id}/approve`),
+        { approve: 0 },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        if (response.status !== 200)
+          throw new Error(`Falha ao reprovar o pedido de doação.`);
+        alert(`Pedido de doação ${id} reprovado com sucesso!`);
+        window.location.reload();
       })
       .catch((error) => {
         alert(error.message);
--- src/components/Tables/SchoolDeviceRequestsTable.js
+++ src/components/Tables/SchoolDeviceRequestsTable.js
@@ -1,11 +1,13 @@
 import { Row } from "react-bootstrap";
-import { ViewScheduleModal } from "../Modals/Schedule/ViewScheduleModal";
-import { EditScheduleModal } from "../Modals/Schedule/EditScheduleModal";
-import { DeleteScheduleModal } from "../Modals/Schedule/DeleteScheduleModal";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark, faEye, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEye,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import { validateDate } from "../../validations/validateDate";
+import { faThumbsDown, faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import SchoolDeviceRequets from "../../classes/SchoolDeviceRequests";
 
 export default function SchoolDeviceRequestsTable(props) {
   const navigate = useNavigate();
@@ -19,11 +21,30 @@ export default function SchoolDeviceRequestsTable(props) {
   }
 
   function currentUserRoleDevicesRoute(userRole, id) {
-    return userRole === "Admin" ? `/admin/devices/${id}` : `/user/devices/${id}`;
+    return userRole === "Admin"
+      ? `/admin/devices/${id}`
+      : `/user/devices/${id}`;
   }
 
-  function handleAdminActions(userRole) {
-    if(userRole === "Admin") return <FontAwesomeIcon icon={faTrash} onClick={() => alert('delete')} />;
+  function handleAdminActions(userRole, id) {
+    if (userRole === "Admin") {
+      return (
+        <>
+        <FontAwesomeIcon
+          icon={faThumbsUp}
+          onClick={() => SchoolDeviceRequets.approve(id)}
+        />
+        <FontAwesomeIcon
+          icon={faThumbsDown}
+          onClick={() => SchoolDeviceRequets.reprove(id)}
+        />
+        <FontAwesomeIcon
+          icon={faTrash}
+          onClick={() => SchoolDeviceRequets.delete(id)}
+        />
+      </>
+      )
+    }
   }
 
   if (props.requestes !== "") {
@@ -83,10 +104,18 @@ export default function SchoolDeviceRequestsTable(props) {
                     </a>
                   </td>
                   <td>
-                    <a href="#" onClick={(event) => {
-                      event.preventDefault();
-                      navigate(currentUserRoleDevicesRoute(props.userRole, request.deviceId));
-                    }}>{`${request.device.brand.name} ${request.device.model.name}`}</a>
+                    <a
+                      href="#"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        navigate(
+                          currentUserRoleDevicesRoute(
+                            props.userRole,
+                            request.deviceId
+                          )
+                        );
+                      }}
+                    >{`${request.device.brand.name} ${request.device.model.name}`}</a>
                   </td>
                   <td>{request.status}</td>
                   <td>{request.accepted ? "sim" : "não"}</td>
@@ -100,10 +129,12 @@ export default function SchoolDeviceRequestsTable(props) {
                       ? validateDate(request.dateColected)
                       : "não definida"}
                   </td>
-                  <td className="d-flex justify-content-start gap-2 align-items-center p-3">
-                    <FontAwesomeIcon icon={faEye} onClick={() => alert('Open single view')}/>
-                    <FontAwesomeIcon icon={faCircleXmark} onClick={() => alert('Cancel action')} />
-                    { handleAdminActions(props.userRole) }
+                  <td className="d-flex justify-content-center gap-3 align-items-center p-3" style={{fontSize: "1.2em"}}>
+                    <FontAwesomeIcon
+                      icon={faEye}
+                      onClick={() => alert("Open single view")}
+                    />
+                    {handleAdminActions(props.userRole, request.id)}
                   </td>
                 </tr>
               );
