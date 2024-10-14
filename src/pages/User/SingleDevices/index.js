54ab58269ce23a722834c8d1bd343393c392fa3e
sandroschutt <dev.sandroschutt@gmail.com>
2024-10-14 13:58:28 -0300

    Fix school device list and single device view

--- src/pages/User/SingleDevices/index.js
+++ src/pages/User/SingleDevices/index.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useUserDataContext } from "../../../context/UserDataContext";
 import { useParams } from "react-router-dom";
 import Device from "../../../classes/Device";
-import SingleDeviceClient from "../../../components/Device/SingleDeviceClient"; 
 import SingleDeviceCard from "../../../components/Device/SingleDeviceCard"; 
 import { Row } from "react-bootstrap";
 
@@ -17,19 +16,11 @@ export default function SingleDevices() {
     }
   }, [device, params.id]);
 
-  function defineTemplate(device) {
-    if (userData.role !== "Admin" && device.returnProccess !== undefined) {
-      return (
-        <SingleDeviceClient description={device.returnProccess.description} />
-      );
-    }
-  }
   return (
     <Row>
       <h1 className="pb-5">{"Dispositivo"}</h1>
       <div className="single-device">
-        <SingleDeviceCard role={userData.role} device={device} />
-        {defineTemplate(device)}
+        <SingleDeviceCard device={device} />
       </div>
     </Row>
   );
